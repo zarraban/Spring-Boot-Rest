0ad82b149b456d1ecf7bca1755121898429bf625
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-27 17:31:51 +0300

    Add tests generated by Spring boot

---
+++ src/test/java/io/github/zarraban/Spring_Boot_Rest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zarraban.Spring_Boot_Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
